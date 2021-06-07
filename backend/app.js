c55bfa89ce6fc1fa9a89689e2ea80439609f3aa0
Morin <morin.alexandre1@gmail.com>
2021-06-07 18:37:29 +0200

    body-parser added

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const mongoose = require('mongoose');
@@ -8,6 +9,19 @@ mongoose.connect('mongodb+srv://Alex18:Mongalex18!@cluster0.m4ibg.mongodb.net/my
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.post('/api/auth/signup', (req, res, next) =>{
+    console.log(req.body);
+});
 
 app.use((req, res) =>{
 res.json({Message: "Votre requête a bien été reçue héhé !"});
