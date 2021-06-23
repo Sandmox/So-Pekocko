ab6429380fd2e29d081e325c9aff6fc756af130c
Morin <morin.alexandre1@gmail.com>
2021-06-23 09:15:42 +0200

    multer added

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const path = require("path");
 
 
 const Sauce = require('./models/sauces');
@@ -25,6 +26,8 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
+
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', saucesRoutes);
--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,37 +1,23 @@
 const Sauce = require('../models/sauces');
 
-/*exports.createSauce = (req, res, next) => {
+exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
+    console.log(sauceObject);
+    delete sauceObject.userId;
+    console.log(sauceObject);
     const sauce = new Sauce({
       ...sauceObject,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
     });
-    thing.save()
+    console.log(sauce);
+    sauce.save()
       .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
       .catch(error => res.status(400).json({ error }));
-  };*/
-
-exports.createSauce = (req, res, next) => {
-    //const newSauce = new Sauces (JSON.parse(req.body));
-    const sauce = new Sauce (req.body);
-    console.log(sauce);
-    sauce.save();
-    res.status(201).json({ message: "Objet enregistré !!" })
+  };
 
 
-    //.catch((error) => res.status(400).json({ error }));
-    /*const newSauce = JSON.parse(req.body.Sauce);
-    console.log(newSauce);
-    newSauce.save();
-    then(() => res.status(201).json({ message: "Objet enregistré !" }))
-    .catch(error => res.status(500).json({error}));*/
-};
 
 
-/*exports.getAllSauces =(req, res, next) => {
-    res.status(200).json({Message: "Tout est ok"});
-};*/
 
 
  
